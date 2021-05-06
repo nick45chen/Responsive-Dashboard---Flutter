eb5e7e773fd1616152127c5c648fd3bc152878f5
nickchen <nick45chen@gmail.com>
2021-05-07 00:53:58 +0800

    update main_screen.dart import side_menu.dart

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,37 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'components/side_menu.dart';
+
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
-class DrawerListTitle extends StatelessWidget {
-  final String title, svgSrc;
-  final VoidCallback onTap;
-
-  const DrawerListTitle({
-    Key key,
-    @required this.title,
-    @required this.svgSrc,
-    this.onTap,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: this.onTap,
-      horizontalTitleGap: 0.0,
-      leading: SvgPicture.asset(
-        this.svgSrc,
-        color: Colors.white54,
-        height: 16,
-      ),
-      title: Text(
-        this.title,
-        style: TextStyle(
-          color: Colors.white54,
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: SideMenu(),
+            ),
+            // It takes 5/6 part of screen
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
         ),
       ),
     );
