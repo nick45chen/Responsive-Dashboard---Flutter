c2f2c1612e2346e67de8056dae9e4e43be5b7ee0
nickchen <nick45chen@gmail.com>
2021-05-07 00:46:54 +0800

    add DrawerListTitle widget

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -6,3 +6,34 @@ class MainScreen extends StatelessWidget {
     return Scaffold();
   }
 }
+
+class DrawerListTitle extends StatelessWidget {
+  final String title, svgSrc;
+  final VoidCallback onTap;
+
+  const DrawerListTitle({
+    Key key,
+    @required this.title,
+    @required this.svgSrc,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: this.onTap,
+      horizontalTitleGap: 0.0,
+      leading: SvgPicture.asset(
+        this.svgSrc,
+        color: Colors.white54,
+        height: 16,
+      ),
+      title: Text(
+        this.title,
+        style: TextStyle(
+          color: Colors.white54,
+        ),
+      ),
+    );
+  }
+}
