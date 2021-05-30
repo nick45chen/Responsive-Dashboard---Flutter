8eeb6abde79230a7b979bdbb553ea56acea127d3
nickchen <nick45chen@gmail.com>
2021-05-30 17:18:49 +0800

    update storageDetail block of dashboard_screen.dart

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:admin/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'components/header.dart';
+import 'components/storage_details.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
@@ -12,6 +13,24 @@ class DashboardScreen extends StatelessWidget {
         child: Column(
           children: [
             Header(),
+            SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 500,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 2,
+                  child: StorageDetails(),
+                ),
+              ],
+            )
           ],
         ),
       ),
